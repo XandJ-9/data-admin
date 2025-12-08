0e1b495619c953a581681b0d43749833aa0bdaa4
admin <admin@example.com>
2025-12-08 11:01:48 +0800

    修改前端异常关闭，但token未删除导致下次登陆异常判断，无法正常跳转到登录界面的问题

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,15 +1,16 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
+const TokenKey = 'Admin-Token' + import.meta.env.VITE_APP_BASE_URL.replace(/\//g, '-')
 
 export function getToken() {
-    if (window.location.pathname.startsWith(import.meta.env.VITE_APP_BASE_URL)) {
-        return Cookies.get(TokenKey)
-    }
-    return
+    return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
+    // 如果已经有同名token，先删除
+    if (getToken()) {
+        removeToken()
+    }
     return Cookies.set(TokenKey, token, { expires: 7, path: import.meta.env.VITE_APP_BASE_URL })
 }
 
