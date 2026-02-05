d528db6f37b86f0f2f12729ec86042043a681b90
xujia <18352538350@163.com>
2026-02-06 02:51:28 +0800

    update

---
+++ backend/apps/dataintegration/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DataintegrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.dataintegration'
+    verbose_name = '数据集成'
