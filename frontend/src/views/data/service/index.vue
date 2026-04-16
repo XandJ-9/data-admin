dcdc46dd9bdd98233b31acdbbacdf05a1aabea1b
xujia <18352538350@163.com>
2026-04-17 02:03:42 +0800

    fix(ui): 数据服务概览命名统一并修复面包屑首页前缀

--- frontend/src/views/data/service/index.vue
+++ frontend/src/views/data/service/index.vue
@@ -2,10 +2,10 @@
   <div class="app-container">
     <el-card>
       <template #header>
-        <span>数据服务</span>
+        <span>服务概览</span>
       </template>
       <p>此模块提供独立的数据查询服务页面。</p>
-      <el-button type="primary" @click="$router.push('/dataservice/query')">进入查询</el-button>
+      <el-button type="primary" @click="$router.push('/data-service/query')">进入查询</el-button>
     </el-card>
   </div>
 </template>
